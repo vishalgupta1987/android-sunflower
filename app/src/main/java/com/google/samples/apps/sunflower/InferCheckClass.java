8cfa707df57b926073e9ce86327add0d661deec8
vishal <vishal.gupta@tokopedia.com>
2019-01-18 15:33:29 +0530

    added changes to test infer

---
+++ app/src/main/java/com/google/samples/apps/sunflower/InferCheckClass.java
@@ -0,0 +1,14 @@
+package com.google.samples.apps.sunflower;
+
+import java.util.List;
+
+public class InferCheckClass {
+
+    public void init() {
+        getListData().size();
+    }
+
+    public static List getListData() {
+        return null;
+    }
+}
